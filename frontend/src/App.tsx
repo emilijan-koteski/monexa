47487555611af89ab7355133907e3d138f373a0c
Emilijan Koteski <emilijankoteski@pm.me>
2025-10-05 13:53:44 +0200

    (fe) Added MUI x-date-pickers and date-fns libraries

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import theme from './theme/theme.ts';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers';
 import AppRoutes from './routes/routes.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,12 +16,14 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>
-        <AppRoutes/>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <AppRoutes/>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </LocalizationProvider>
   );
 }
 
