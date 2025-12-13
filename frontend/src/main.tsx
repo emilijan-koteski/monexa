6d832a589af8e60aa88a6d92f959d19b0768098e
Emilijan Koteski <emilijankoteski@pm.me>
2025-12-13 17:13:58 +0100

    (fe) Updated fontsource packages; adjusted imports; removed deprecated tsconfig option; updated eslint script

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import '@fontsource/tilt-neon';
-import '@fontsource/roboto';
+import '@fontsource/tilt-neon/index.css';
+import '@fontsource/roboto/index.css';
 import './i18n.ts';
 import App from './App.tsx';
 
