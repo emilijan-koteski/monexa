518da39ada0bef0b5957210f2cbacae09958ec15
Emilijan Koteski <emilijankoteski@pm.me>
2026-03-06 17:17:37 +0100

    Added Macedonian password reset email template and updated mail service to support localized templates and subjects.

--- internal/services/mail_service.go
+++ internal/services/mail_service.go
@@ -1,8 +1,22 @@
 package services
 
 import (
+	"path/filepath"
+
 	"github.com/emilijan-koteski/monexa/internal/clients"
+	"github.com/emilijan-koteski/monexa/internal/models/types"
 )
+
+const (
+	PasswordResetTemplate = "templates/email/password_reset.html"
+)
+
+var emailSubjects = map[string]map[types.LanguageType]string{
+	PasswordResetTemplate: {
+		types.EnglishLanguage:    "Reset your password",
+		types.MacedonianLanguage: "Ресетирајте ја вашата лозинка",
+	},
+}
 
 type MailService struct {
 	mailClient *clients.MailClient
@@ -18,4 +32,22 @@ func (s *MailService) SendText(to, subject, body string) error {
 
 func (s *MailService) SendHTML(to, subject, htmlBody string) error {
 	return s.mailClient.SendHTML(to, subject, htmlBody)
+}
+
+func (s *MailService) GetEmailTemplatePath(template string, language types.LanguageType) string {
+	if language == types.MacedonianLanguage {
+		dir := filepath.Dir(template)
+		file := filepath.Base(template)
+		return filepath.Join(dir, "mk", file)
+	}
+	return template
+}
+
+func (s *MailService) GetEmailSubject(template string, language types.LanguageType) string {
+	if subjects, ok := emailSubjects[template]; ok {
+		if subject, ok := subjects[language]; ok {
+			return subject
+		}
+	}
+	return ""
 }
